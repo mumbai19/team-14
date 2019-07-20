7a04d854e7d5cf90875bfc92c035172360dc92d8
darshan761 <darshanpatil761@gmail.com>
2019-07-20 20:43:42 +0530

    training added

--- training_home.php
+++ training_home.php
@@ -1,22 +1,23 @@
 
 
 
-<button href="training.php">Fishing Techniques</button>
+
 
 <?php
 session_start();
 include('includes/config.php');
 // $uid=$_SESSION['id'];
-if(isset($_POST['training']))
-{
-    $id = $_POST['training'];
 $sql = "SELECT *  FROM module";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-        echo $row["name"];
+    while($row = $result->fetch_assoc()) { 
+         echo $row["module_name"];
    }
-}
+// if(isset($_POST['training']))
+// {
+//     $id = $_POST['training'];
+
+// }
 }
     
 ?>
