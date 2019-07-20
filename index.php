9c4a390fb314afe374fb61628990683703e9ade4
janyanisanjay <sanjayjanyani43@gmail.com>
2019-07-20 17:03:49 +0530

    landing page n folder structure

---
+++ index.php
@@ -0,0 +1,337 @@
+<html lang="en">
+
+<head>
+
+    <!--TITLE OF THE PAGE-->
+    <title>Jaljeevika</title>
+
+    <link rel="shortcut icon" href="img/icon.jpg">
+
+    <!--Fontawesome 4.7.0-->
+    <link rel="stylesheet" href="vendors/fontawesome/css/font-awesome.min.css">
+
+    <!--Fonts Used-->
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i" rel="stylesheet">
+    
+    
+    <!--core plugin css-->
+    <link rel="stylesheet" href="vendors/bootstrap/css/bootstrap.min.css">
+    <!-- end of core plugin css-->
+    
+    <!--ANIMATE CSS-->
+    <link rel="stylesheet" href="vendors/animate/animate.css">
+    <!--END OF ANIMATE CSS-->
+    
+    <!--MAGNIFY CSS-->
+    <link rel="stylesheet" href="vendors/magnify/magnific-popup.css">
+    
+    <!--Costom Styling css-->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/responsive.css">
+    <!-- End of Costom Styling css-->
+</head>
+
+<body class="text-content" data-spy="scroll" data-target=".navbar-inverse" data-offset="65">
+   <!--NAVIGATION-->
+   <nav class="navbar navbar-inverse navbar-fixed-top " role="navigation">
+       <div class="container-fluid">
+           <div class="wg-nav-wrapper">
+               <div class="nav-header">
+                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#wg-menu">
+                      <span class="sr-only">Toggle navigation</span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                  </button>
+                   <a href="#" class="navbar-brand">JALJEEVIKA</a>
+               </div>
+               <div id="wg-menu" class="collapse navbar-collapse">
+                   <ul class="nav navbar-nav">
+                       <li><a class="smooth-scroll" href="#home">Home</a></li>
+                       <li><a class="smooth-scroll" href="#about">Training</a></li>
+                       <li><a class="smooth-scroll" href="#work">Ponds</a></li>
+                       <li><a class="smooth-scroll" href="#testimonials">Schemes</a></li>
+                       <li><a class="smooth-scroll" href="#pricing">Seeds</a></li>
+                       <li><a class="smooth-scroll" href="#stats">Fishes</a></li>
+                       <li><a class="smooth-scroll" href="#clients">Sign Up</a></li>
+                       <li><a class="smooth-scroll" href="#contact">Contact</a></li>
+                       <li><a class="smooth-scroll" href="#clients">LOGIN</a></li>
+                   </ul>
+               </div>
+           </div>
+       </div>
+   </nav>
+   <!--HOME SECTION-->
+   <section id="home" class="home-section">
+       <div class="home-cover bg-parallax wow animated fadeIn">
+           <div class="home-content-box">
+               <div class="home-content-box-inner text-center">
+                   <div class="home-heading">
+                       <h3 class="text-heading wow animated zoomIn">JALJEEVIKA<br/>Transforming Lives</h3>
+                   </div><!--.home-heading-->
+                   <div class="home-btn wow animated zoomIn">
+                       <a href="#about" class="btn btn-lg btn-general btn-white smooth-scroll" role="button" title="View Our Work">View Our Work</a>
+                   </div>
+               </div>
+           </div>
+       </div>
+   </section>
+   <!--END OF HOME SECTION-->
+   
+   <!--SERVICES SECTION-->
+   <section id="services">
+       <div class="content-box">
+           <div class="content-title wow animated fadeInDown" data-wow-duration="1s" data-wow-delay=".3s">
+               <h3 class="text-heading">Services</h3>
+               <div class="content-title-underline"></div>
+           </div><!--.content-title-->
+           <div class="container">
+               <div class="row wow animated fadeInUp" data-wow-duration="1s" data-wow-delay=".3s">
+                  <!--SERVICE ITEM 1-->
+                   <div class="col-md-4 col-sm-6">
+                     <div class="service-item">
+                        <div class="service-item-icon">
+                            <i class="fa fa-paint-brush fa-3x"></i>
+                        </div>
+                        <div class="service-item-title">
+                           <h3 class="text-heading">Web Design</h3> 
+                        </div> 
+                        <div class="service-item-desc">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                         </div>
+                     </div>
+                   </div>
+                   <div class="col-md-4 col-sm-6">
+                     <div class="service-item">
+                        <div class="service-item-icon">
+                            <i class="fa fa-laptop fa-3x"></i>
+                        </div> 
+                        <div class="service-item-title">
+                           <h3 class="text-heading">Web Development</h3> 
+                        </div> 
+                        <div class="service-item-desc">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </div>
+                     </div>
+                   </div>
+                   <!--END OF SERVICE ITEM 2-->
+                    <!--SERVICE ITEM 3-->
+                    <div class="col-md-4 col-sm-6">
+                     <div class="service-item">
+                        <div class="service-item-icon">
+                            <i class="fa fa-tablet fa-3x"></i>
+                        </div><!--.service-item-icon--> 
+                        <div class="service-item-title">
+                           <h3 class="text-heading">Mobile App</h3> 
+                        </div> <!--.service-item-title-->
+                        <div class="service-item-desc">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </div><!--.service-item-desc-->
+                     </div><!--.service-item--> 
+                   </div><!--.col-md-4-->
+                    <!--END OF SERVICE ITEM 3-->
+                    <!--SERVICE ITEM 4-->
+                    <div class="col-md-4 col-sm-6">
+                     <div class="service-item">
+                        <div class="service-item-icon">
+                            <i class="fa fa-search fa-3x"></i>
+                        </div><!--.service-item-icon--> 
+                        <div class="service-item-title">
+                           <h3 class="text-heading">Seo</h3> 
+                        </div> <!--.service-item-title-->
+                        <div class="service-item-desc">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </div><!--.service-item-desc-->
+                     </div><!--.service-item--> 
+                   </div><!--.col-md-4-->
+                    <!--END OF SERVICE ITEM 4-->
+                    <!--SERVICE ITEM 5-->
+                    <div class="col-md-4 col-sm-6">
+                     <div class="service-item">
+                        <div class="service-item-icon">
+                            <i class="fa fa-pencil-square-o fa-3x"></i>
+                        </div><!--.service-item-icon--> 
+                        <div class="service-item-title">
+                           <h3 class="text-heading">Ux Design</h3> 
+                        </div> <!--.service-item-title-->
+                        <div class="service-item-desc">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </div><!--.service-item-desc-->
+                     </div><!--.service-item--> 
+                   </div><!--.col-md-4-->
+                    <!--END OF SERVICE ITEM 5-->
+                    <!--SERVICE ITEM 6-->
+                    <div class="col-md-4 col-sm-6">
+                     <div class="service-item">
+                        <div class="service-item-icon">
+                            <i class="fa fa-support fa-3x"></i>
+                        </div><!--.service-item-icon--> 
+                        <div class="service-item-title">
+                           <h3 class="text-heading">Support</h3> 
+                        </div> <!--.service-item-title-->
+                        <div class="service-item-desc">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </div><!--.service-item-desc-->
+                     </div><!--.service-item--> 
+                   </div><!--.col-md-4-->
+                    <!--END OF SERVICE ITEM 6-->
+
+
+               </div><!--.row-->
+           </div><!--.container-->
+       </div>
+   </section>
+   <!-- END OF SERVICES SECTION-->
+   
+    
+    
+   
+   <!--STATS SECTION-->
+   <section id="stats">
+       <div class="stats-cover bg-parallax">
+           <div class="content-box">
+           <div class="content-title wow animated fadeInDown" data-wow-duration="1s" data-wow-delay=".3s">
+               <h3 class="text-heading text-heading-white">We never stop improving</h3>
+                <div class="content-title-underline"></div>
+           </div><!--.content-title-->
+           <div class="container">
+               <div class="row wow animated fadeInUp" data-wow-duration="1s" data-wow-delay=".3s">
+                 <div class="col-md-3 col-sm-6">
+                     <div class="stats-item">
+                         <i class="fa fa-cloud-download fa-5x"></i>
+                         <h2><span class="counter">1590</span>+</h2>
+                         <p>Downloads</p>
+                     </div>
+                 </div> 
+                 
+                  <div class="col-md-3 col-sm-6">
+                     <div class="stats-item">
+                         <i class="fa fa-star-o fa-5x"></i>
+                         <h2><span class="counter">3500</span>+</h2>
+                         <p>Awards</p>
+                     </div>
+                 </div>  
+                 
+                  <div class="col-md-3 col-sm-6">
+                     <div class="stats-item">
+                         <i class="fa fa-heart-o fa-5x"></i>
+                         <h2><span class="counter">1199</span>+</h2>
+                         <p>Likes</p>
+                     </div>
+                 </div>  
+                 
+                  <div class="col-md-3 col-sm-6">
+                     <div class="stats-item">
+                         <i class="fa fa-check fa-5x"></i>
+                         <h2><span class="counter">2200</span>+</h2>
+                         <p>Recommended</p>
+                     </div>
+                 </div>   
+               </div><!--.row-->
+           </div><!--.container-->
+       </div><!--.content-box-->
+       </div>
+   </section>
+   <!--END OF STATS SECTION-->
+       <footer id="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="contact-left">
+                      <h3>White Graphics</h3>
+                      <p>We believe in <strong>Simple</strong> , <strong>Clean</strong> &amp; <strong>Modern</strong> Design Standards with Responsive Approach. Browse the amazing work of our company.</p> 
+                      <div class="contact-info">
+                          <address>
+                              <strong>Headquaters:</strong>
+                              <p>313, Evergreen CHS.<br>
+                              Airoli Sector 15,<br>
+                              New Bombay,<br>
+                              Mumbai - 55.
+                              </p>
+                          </address>
+                          <div class="phone-fax-email">
+                             <p>
+                              <strong>Phone:</strong> <span>(719)-778-8804</span>
+                              <br/>
+                              <strong>Fax:</strong> <span>(719)-778-8804 8890</span>
+                              <br/>
+                              <strong>Email:</strong> <span>info@whitegrapphics.in</span>
+                              <br/> 
+                              </p>
+                          </div>
+                      </div> 
+                      <ul class="social-list">
+                          <li><a href="" class="social-icon icon-white icon-facebook"><i class="fa fa-facebook"></i></a></li>
+                          <li><a href="" class="social-icon icon-white icon-twitter"><i class="fa fa-twitter"></i></a></li>
+                          <li><a href="" class="social-icon icon-white icon-google-plus"><i class="fa fa-google-plus"></i></a></li>
+                          <li><a href="" class="social-icon icon-white icon-youtube"><i class="fa fa-youtube"></i></a></li>
+                      </ul>
+                    </div>
+                </div>
+                
+                <div class="col-md-6">
+                    <div class="contact-right">
+                        <h3>Contact Us</h3>
+                        <form action="#">
+                            <input type="text" name="full-name" placeholder="Full Name" class="form-control">
+                            <input type="email" name="email" placeholder="Email Address" class="form-control">
+                            <textarea name="message" rows="3" placeholder="Your Message..." class="form-control"></textarea>
+                            
+                            <div class="send-btn">
+                                <a href="#" class="btn btn-lg btn-general btn-white" role="button">Send</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="copyrights">
+                    <div class="row ">
+                        <div class="col-md-6">
+                            <p class="copyright">Copyrights @ White Graphics, 2018</p>
+                        </div><!--.col-md-6-->
+                        <div class="col-md-6 links hidden-sm hidden-xs">
+                            <a href="#" class="bottom-links">Home </a><span>/ </span>
+                            <a href="#" class="bottom-links">Services </a><span>/ </span>
+                            <a href="#" class="bottom-links">About </a><span>/ </span>
+                            <a href="#" class="bottom-links">Work </a><span>/ </span>
+                            <a href="#" class="bottom-links">Team </a><span>/ </span>
+                            <a href="#" class="bottom-links">Pricing </a><span>/ </span>
+                            <a href="#" class="bottom-links">Clients </a>
+                        </div><!--.col-md-6-->
+                    </div><!--.row-->
+            </div><!--copyrights-->
+        
+        <a href="#home" class="smooth-scroll btn btn-am btn-blue btn-back-to-top hidden-xs hidden-sm" title="home" role="button"><i class="fa fa-angle-up"></i></a>
+    </footer>
+   
+   
+   <!--SCRIPTS STARTS HERE-->
+    
+    <!--CORE JS-->
+    
+    <!--jQuery Script-->
+    <script src="vendors/jquery/jquery-3.3.1.min.js"></script>
+    <!--Bootstrap Script-->
+    <script src="vendors/bootstrap/js/bootstrap.min.js"></script>
+    
+    <!--wow.js-->
+
+    <!--COUNTERUP-->
+    <script src="vendors/waypoints/jquery.waypoints.min.js"></script>
+    <script src="vendors/counterup/jquery.counterup.min.js"></script>
+    
+    <!--Easing-->
+    <script src="vendors/easing/easing.js"></script>
+    
+    <!--END OF CORE JS-->
+    
+    <!--CUSTOM JS-->
+    <script src="js/script.js"></script>
+    <!--END OF CUSTOM JS-->
+    
+    <!--SCRIPTS ENDS HERE-->
+
+</body>
+
+</html>
