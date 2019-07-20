89e1b8a678cf11def5572e3f577c876e61946891
Amruta Parab <amruta0303@gmail.com>
2019-07-20 18:09:57 +0530

    registration form added

---
+++ registration.php
@@ -0,0 +1,263 @@
+<html lang="en">
+
+<head>
+
+    <!--TITLE OF THE PAGE-->
+    <title>Jaljeevika</title>
+
+    <link rel="shortcut icon" href="img/icon.jpg">
+
+    <!--Fontawesome 4.7.0-->
+    <link rel="stylesheet" href="vendors/fontawesome/css/font-awesome.min.css">
+
+    <!--Fonts Used-->
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i" rel="stylesheet">
+    
+    
+    <!--core plugin css-->
+    <link rel="stylesheet" href="vendors/bootstrap/css/bootstrap.min.css">
+    <!-- end of core plugin css-->
+    
+    <!--ANIMATE CSS-->
+    <link rel="stylesheet" href="vendors/animate/animate.css">
+    <!--END OF ANIMATE CSS-->
+    
+    <!--MAGNIFY CSS-->
+    <link rel="stylesheet" href="vendors/magnify/magnific-popup.css">
+    
+    <!--Costom Styling css-->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/responsive.css">
+    <!-- End of Costom Styling css-->
+</head>
+
+<body class="text-content" data-spy="scroll" data-target=".navbar-inverse" data-offset="65">
+   <!--NAVIGATION-->
+   <nav class="navbar navbar-inverse navbar-fixed-top " role="navigation">
+       <div class="container-fluid">
+           <div class="wg-nav-wrapper">
+               <div class="nav-header">
+                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#wg-menu">
+                      <span class="sr-only">Toggle navigation</span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                  </button>
+                   <a href="#" class="navbar-brand">JALJEEVIKA</a>
+               </div>
+               <div id="wg-menu" class="collapse navbar-collapse">
+                   <ul class="nav navbar-nav">
+                       <li><a class="smooth-scroll" href="#home">Home</a></li>
+                       <li><a class="smooth-scroll" href="#about">Training</a></li>
+                       <li><a class="smooth-scroll" href="#work">Ponds</a></li>
+                       <li><a class="smooth-scroll" href="#testimonials">Schemes</a></li>
+                       <li><a class="smooth-scroll" href="#pricing">Seeds</a></li>
+                       <li><a class="smooth-scroll" href="#stats">Fishes</a></li>
+                       <li><a class="smooth-scroll" href="#clients">Sign Up</a></li>
+                       <li><a class="smooth-scroll" href="#contact">Contact</a></li>
+                       <li><a class="smooth-scroll" href="#clients">LOGIN</a></li>
+                   </ul>
+               </div>
+           </div>
+       </div>
+   </nav>
+
+<br><br><br><br><br><br>
+<div class="container">
+<!-- Default form login -->
+<div class="col-md-5 mx-auto">
+<form class="text-center border border-light p-5" method="POST">
+
+    <p class="h4 mb-4">Sign Up</p>
+
+	<!-- Name -->
+    <input type="text" id="defaultLoginFormName" name="name" class="form-control mb-4" placeholder="Name">
+
+	<!-- Username -->
+    <input type="text" id="defaultLoginFormUsername" name="username" class="form-control mb-4" placeholder="Username">
+
+    <!-- Email -->
+    <input type="email" id="defaultLoginFormEmail" name="emailid" class="form-control mb-4" placeholder="E-mail">
+
+	<!-- Password -->
+    <input type="password" id="defaultLoginFormPassword" name="password" class="form-control mb-4" placeholder="Password">
+	
+	<!-- Phone Number -->
+    <input type="text" id="defaultLoginFormPhone" name="phone" class="form-control mb-4" placeholder="Phone No.">
+
+	<!-- Address -->
+    <input type="text" id="defaultLoginFormAddressLine1" name="line1" class="form-control mb-4" placeholder="Address Line 1">
+	<input type="text" id="defaultLoginFormAddressLine2" name="line2" class="form-control mb-4" placeholder="Address Line 2">
+	<div class="form-row mb-4">
+        <div class="col">
+            <!-- City -->
+            <input type="text" id="defaultRegisterFormCity" class="form-control" placeholder="City">
+        </div>
+        <div class="col">
+            <!-- District -->
+            <input type="text" id="defaultRegisterFormDistrict" class="form-control" placeholder="District">
+        </div>
+    </div>
+	<div class="form-row mb-4">
+		<div class="col">
+            <!-- State -->
+            <input type="text" id="defaultRegisterFormState" class="form-control" placeholder="State">
+        </div>
+        <div class="col">
+            <!-- District -->
+            <input type="text" id="defaultRegisterFormPincode" class="form-control" placeholder="Pincode">
+        </div>
+    </div>
+
+
+	Login As :<br>
+	<div class="form-row mb-4">
+		<div class="col">
+			<div class="custom-control custom-radio">
+			<input type="radio" class="custom-control-input" id="defaultUnchecked" name="defaultExampleRadios">
+			<label class="custom-control-label" for="defaultUnchecked">Fisherman</label>
+			</div>
+        </div>
+        <div class="col">
+			<div class="custom-control custom-radio">
+			<input type="radio" class="custom-control-input" id="defaultChecked" name="defaultExampleRadios" checked>
+			<label class="custom-control-label" for="defaultChecked">Expert</label>
+			</div>
+        </div>
+    </div>
+	<div class="form-row mb-4">
+		<div class="col">
+			<div class="custom-control custom-radio">
+			<input type="radio" class="custom-control-input" id="defaultUnchecked" name="defaultExampleRadios">
+			<label class="custom-control-label" for="defaultUnchecked">Vendor</label>
+			</div>
+        </div>
+        <div class="col">
+			<div class="custom-control custom-radio">
+			<input type="radio" class="custom-control-input" id="defaultChecked" name="defaultExampleRadios" checked>
+			<label class="custom-control-label" for="defaultChecked">Pond Owner</label>
+			</div>
+        </div>
+    </div>
+	
+	<div id="message"></div>
+
+    <!-- Sign in button -->
+    <button class="btn btn-info btn-block my-4" type="submit" name="submit">Register</button>
+
+    <!-- Register -->
+    <p>Already have an account?
+        <a href="login.php">Sign In</a>
+    </p>
+
+</form>
+</div>
+<!-- Default form login -->
+</div>
+ 
+
+
+   
+  
+       <footer id="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="contact-left">
+                      <h3>White Graphics</h3>
+                      <p>We believe in <strong>Simple</strong> , <strong>Clean</strong> &amp; <strong>Modern</strong> Design Standards with Responsive Approach. Browse the amazing work of our company.</p> 
+                      <div class="contact-info">
+                          <address>
+                              <strong>Headquaters:</strong>
+                              <p>313, Evergreen CHS.<br>
+                              Airoli Sector 15,<br>
+                              New Bombay,<br>
+                              Mumbai - 55.
+                              </p>
+                          </address>
+                          <div class="phone-fax-email">
+                             <p>
+                              <strong>Phone:</strong> <span>(719)-778-8804</span>
+                              <br/>
+                              <strong>Fax:</strong> <span>(719)-778-8804 8890</span>
+                              <br/>
+                              <strong>Email:</strong> <span>info@whitegrapphics.in</span>
+                              <br/> 
+                              </p>
+                          </div>
+                      </div> 
+                      <ul class="social-list">
+                          <li><a href="" class="social-icon icon-white icon-facebook"><i class="fa fa-facebook"></i></a></li>
+                          <li><a href="" class="social-icon icon-white icon-twitter"><i class="fa fa-twitter"></i></a></li>
+                          <li><a href="" class="social-icon icon-white icon-google-plus"><i class="fa fa-google-plus"></i></a></li>
+                          <li><a href="" class="social-icon icon-white icon-youtube"><i class="fa fa-youtube"></i></a></li>
+                      </ul>
+                    </div>
+                </div>
+                
+                <div class="col-md-6">
+                    <div class="contact-right">
+                        <h3>Contact Us</h3>
+                        <form action="#">
+                            <input type="text" name="full-name" placeholder="Full Name" class="form-control">
+                            <input type="email" name="email" placeholder="Email Address" class="form-control">
+                            <textarea name="message" rows="3" placeholder="Your Message..." class="form-control"></textarea>
+                            
+                            <div class="send-btn">
+                                <a href="#" class="btn btn-lg btn-general btn-white" role="button">Send</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="copyrights">
+                    <div class="row ">
+                        <div class="col-md-6">
+                            <p class="copyright">Copyrights @ White Graphics, 2018</p>
+                        </div><!--.col-md-6-->
+                        <div class="col-md-6 links hidden-sm hidden-xs">
+                            <a href="#" class="bottom-links">Home </a><span>/ </span>
+                            <a href="#" class="bottom-links">Services </a><span>/ </span>
+                            <a href="#" class="bottom-links">About </a><span>/ </span>
+                            <a href="#" class="bottom-links">Work </a><span>/ </span>
+                            <a href="#" class="bottom-links">Team </a><span>/ </span>
+                            <a href="#" class="bottom-links">Pricing </a><span>/ </span>
+                            <a href="#" class="bottom-links">Clients </a>
+                        </div><!--.col-md-6-->
+                    </div><!--.row-->
+            </div><!--copyrights-->
+        
+        <a href="#home" class="smooth-scroll btn btn-am btn-blue btn-back-to-top hidden-xs hidden-sm" title="home" role="button"><i class="fa fa-angle-up"></i></a>
+    </footer>
+   
+   
+   <!--SCRIPTS STARTS HERE-->
+    
+    <!--CORE JS-->
+    
+    <!--jQuery Script-->
+    <script src="vendors/jquery/jquery-3.3.1.min.js"></script>
+    <!--Bootstrap Script-->
+    <script src="vendors/bootstrap/js/bootstrap.min.js"></script>
+    
+    <!--wow.js-->
+
+    <!--COUNTERUP-->
+    <script src="vendors/waypoints/jquery.waypoints.min.js"></script>
+    <script src="vendors/counterup/jquery.counterup.min.js"></script>
+    
+    <!--Easing-->
+    <script src="vendors/easing/easing.js"></script>
+    
+    <!--END OF CORE JS-->
+    
+    <!--CUSTOM JS-->
+    <script src="js/script.js"></script>
+    <!--END OF CUSTOM JS-->
+    
+    <!--SCRIPTS ENDS HERE-->
+
+</body>
+
+</html>
