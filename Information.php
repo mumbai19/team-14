93902408be028d186634f4a6a15891efc37e9447
darshan761 <darshanpatil761@gmail.com>
2019-07-21 06:14:21 +0530

    information page added

---
+++ Information.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  <script>
+  #mydiv {
+   position: fixed;
+   top: 50%;
+   left: 50%;
+   transform: translate(-50%, -50%);
+}
+  </script>
+</head>
+<body style="background-image:url('img/schemes.jpg');  background-repeat: no-repeat;  background-size: cover;">
+<div style="background-color:afeeee" class="jumbotron text-center"><br>
+    <h1 style="font-family:sans-serif" class="display-4">Schemes</h1>
+</div>
+<div style="text-align: center">
+   
+<div style="display:inline-block" class='row'>
+<button style="width:500px" data-toggle="collapse" data-target="#demo5">CENTRALLY SPONSORED PLAN SCHEMES</button>
+
+<div id="demo5" class="collapse">
+<table class="table table-hover">
+    <tbody>
+      <tr>
+        <td>Development of Marine fisheries infrastructure & post-harvest operation</td>
+      </tr>
+      <tr>
+        <td>National scheme for Welfare for Fishermen</td>
+      </tr>
+      <tr>
+        <td>Development of Inland Fisheries & Aquaculture </td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+</div>
+
+<br>
+<div style="display:inline-block" class='row'>
+<button style="width:500px" data-toggle="collapse" data-target="#demo4">STATE PLAN SCHEMES (100% State assistance) </button>
+
+<div id="demo4" class="collapse">
+<table class="table table-hover">
+    <tbody>
+      <tr>
+        <td>Infrastructure improvement</td>
+      </tr>
+      <tr>
+        <td>Mastyajibi Unnayan Yojana</td>
+      </tr>
+      <tr>
+        <td>Contribution towards NFDB assistance</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+</div>
+<br>
+<div style="display:inline-block" class='row'>
+<button style="width:500px" data-toggle="collapse" data-target="#demo3">Central Assistance from outside Budget</button>
+
+<div id="demo3" class="collapse">
+<table class="table table-hover">
+    <tbody>
+      <tr>
+        <td>Intensive Coastal Zone Management Project (ICZMP)
+ </td>
+      </tr>
+      <tr>
+        <td>Orissa Community Tank Management Project (OCTMP)  </td>
+      </tr>
+      <tr>
+        <td>National Fisheries Development Board (NFDB) </td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+</div>
+<br>
+<div style="display:inline-block" class='row'>
+<button style="width:500px" data-toggle="collapse" data-target="#demo2">RIDF (20% state share : 80% NABARD)</button>
+
+<div id="demo2" class="collapse">
+<table class="table table-hover">
+    <tbody>
+      <tr>
+        <td> Aquaculture development through integrated approach in Rayagada district under NMPS
+ </td>
+      </tr>
+      <tr>
+        <td> Assistance to fishermen for livelihood development for marine & Chilika fishermen </td>
+      </tr>
+      <tr>
+        <td>Replacement of breeders & management of fish seed farm</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+</div>
+<br>
+
+<div style="display:inline-block" class='row'>
+
+<button style="width:500px" data-toggle="collapse" data-target="#demo1">MUY (Mastyajibi Unnayan Yojana)</button>
+
+<div id="demo1" class="collapse">
+<table class="table table-hover">
+    <tbody>
+      <tr>
+        <td>Replacement of wooden country craft with FRP boat for marine fishermen under RKVY
+   </td>
+      </tr>
+      <tr>
+        <td> Motorization of country craft for marine fishermen</td>
+      </tr>
+      <tr>
+        <td> Demonstration of cage culture in reservoirs under RKVY</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+</div>
+<div  class='row'>
+<button style="width:500px" data-toggle="collapse" data-target="#demo6">CENTRAL PLAN SCHEMES (100% Central Assistance)</button>
+
+<div  id="demo6" class="collapse">
+<table class="table table-hover">
+    <tbody>
+      <tr>
+        <td>Strengthening of Data base and information net working for Fisheries sector</td>
+      </tr>
+      <tr>
+        <td>Introduction of Intermediate crafts of improved design</td>
+      </tr>
+      <tr>
+        <td>Safety of Fishermen at Sea
+</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+</div>
+
+</div>
+
+</body>
+</html>
